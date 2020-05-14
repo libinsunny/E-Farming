d8a931d6f819da0808c4d744ce96e9b1cba86fe3
libinsunny <libinsunny1996@gmail.com>
2020-05-14 09:52:20 +0530

    first commit

---
+++ efarming/farmer/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+from .import views
+
+
+urlpatterns = [
+    path('farmerhomepage/', views.farmerhomepage, name='farmerhomepage'),
+    path('product/', views.products, name='product'),
+    path('singleproduct/<int:productid>', views.singleproduct, name='singleproduct'),
+    path('viewcart/', views.viewcart, name='viewcart'),
+    path('addtocart/<int:productid>', views.addtocart, name='addtocart'),
+    path('viewapplication/', views.viewapplication, name='viewapplication'),
+    path('viewtechnique/', views.viewtechnique, name='viewtechnique'),
+    path('farmerprofile/', views.farmerprofile, name='farmerprofile'),
+    path('deletecart/<int:productid>', views.deletecart, name='deletecart'),
+    path('seed/', views.seed, name='seed'),
+    path('plant/', views.plant, name='plant'),
+
+
+
+
+
+
+
+
+]
